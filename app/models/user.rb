86c34d9d32743ba1792c7a35c4bbe83d01865ca1
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-02-14 15:26:26 -0500

    working models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :friends, :through => :friendships
+  has_many :likes
   has_many :articles, :through => :likes
 
 
--- bin/app/models/article.rb
+++
@@ -1,4 +0,0 @@
-class Article < ApplicationRecord
-  has_many :interests
-  has_many :likes, :through => :users
-end

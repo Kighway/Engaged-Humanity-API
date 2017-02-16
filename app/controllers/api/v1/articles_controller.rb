98da013989d91f2819db31c8436a80afb4d61477
andigan <andrew.nease.code@gmail.com>
2017-02-16 18:28:24 -0500

    add experimental serializers

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,30 +3,49 @@ module Api
     class ArticlesController < ApplicationController
       def index
 
-        jwt_from_session = request.headers['HTTP_AUTHORIZATION']
-
-        if jwt_from_session.length == 0
-
-          # send back no articles
-          @articles_by_interest = []
-          @articles_by_friends_likes = []
-
-        # else if session data is a jwt string
-        else
+        # jwt_from_session = request.headers['HTTP_AUTHORIZATION']
+        #
+        # if jwt_from_session.length == 0
+        #
+        #   # send back no articles
+        #   @articles_by_interest = []
+        #   @articles_by_friends_likes = []
+        #
+        # # else if session data is a jwt string
+        # else
           # can probably delete this:
           # jwt_user_id = Auth.decode(jwt_from_session)
-          # user = User.find(jwt_user_id["userid"])
+          user = User.find(1)
 
-          user = active_user
+#          user = active_user
+
+
+
 
           # get articles that a user is interested in
           @articles_by_interest = user.interests.collect {|interest| interest.articles}.flatten.uniq
           # get articles that a friend has liked
           @articles_by_friends_likes = user.friends.collect {|friend| friend.articles}.flatten.uniq
-        end
+#        end
 
-        render json: {articles_by_interest: @articles_by_interest, articles_by_friends_likes: @articles_by_friends_likes}
+
+
+        # render json: {articles_by_interest: @articles_by_interest, articles_by_friends_likes: @articles_by_friends_likes}
+
+        # @articles = Article.all
+#        render json: @articles_by_friend_likes
+        render json: @articles_by_interest
+
+
       end
+
+
+
+
+
+
+
+
 
       def show
         @article = Article.find(params[:id])
